bb85baca4ed577bf5dc302f6c844fd6315dec2aa
Axel Dion <axel.dion.y9116@student.hogent.be>
2018-03-05 10:03:09 +0100

    Klassen UC1 aanmaken

---
+++ java-g03/src/domein/Actie.java
@@ -0,0 +1,13 @@
+package domein;
+
+public class Actie {
+    private String naam;
+    private String opgave;
+
+    public Actie(String naam, String opgave) {
+        this.naam = naam;
+        this.opgave = opgave;
+    }
+
+
+}
