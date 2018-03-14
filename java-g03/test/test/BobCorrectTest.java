98b2c819acada4157a473af6fff3643498d2d206
Michiel Leunens <michiel.leunens.y77433@student.hogent.be>
2018-03-15 00:03:03 +0100

    Testen is hopeloos pls send help

--- java-g03/test/test/BobCorrectTest.java
+++ java-g03/test/test/BobCorrectTest.java
@@ -29,7 +29,7 @@ public class BobCorrectTest {
     public static Collection<Object[]> getTestParameters(){
         return Arrays.asList(
                 new Object[][]{
-                        {"Correcte naam", new ArrayList<Oefening>(), new ArrayList<Actie>(), new ArrayList<Toegangscode>()},
+                     //   {"Correcte naam", new ArrayList<Oefening>(Arrays.asList(new Oefening("oefening3", "opgavePath", "antwoord", "feedbackPath", new ArrayList<Groepsbewerking>(Arrays.asList(new Groepsbewerking("groepsbewerking1", "test", "2", Bewerking.AFTREKKING),new Groepsbewerking("groepsbewerking2", "test", "2", Bewerking.AFTREKKING))), new Vak("wiskunde", "red"))), new ArrayList<Actie>(Arrays.asList(new Actie("potje", "Zoek blauw potje"), new Actie("blad", "zoek het gele blad"), new Actie("doos", "zoek de rode doos"))), new ArrayList<Toegangscode>())},
                         {"Correcte Naam", new ArrayList<Oefening>(), new ArrayList<Actie>(), new ArrayList<Toegangscode>()},
                         {"Correcte Naam", new ArrayList<Oefening>(), new ArrayList<Actie>(), new ArrayList<Toegangscode>()},
                 });
